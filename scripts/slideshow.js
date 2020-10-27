ed24e1b33436686fb8cafe5c15b777aef60d790c
DriesDD <driesdedecker@gmail.com>
2020-10-27 17:00:53 +0100

    working on slideshow

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,13 +1,18 @@
 // Automatic Slideshow - change image every 3 seconds
 var myIndex = 0;
-document.addEventListener("DOMContentLoaded", function(){
+
+document.addEventListener("DOMContentLoaded", carousel());
+
+function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     myIndex++;
-    if (myIndex > x.length) { myIndex = 1 }
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
     x[myIndex - 1].style.display = "block";
     setTimeout(carousel, 3000);
 }
