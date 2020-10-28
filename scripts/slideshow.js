5f1ca2b3f588c3c53b5de00e35d77089dc0fd928
DriesDD <driesdedecker@gmail.com>
2020-10-28 10:08:10 +0100

    fix carousel

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,18 +1,16 @@
 // Automatic Slideshow - change image every 3 seconds
-var myIndex = 0;
 
-document.addEventListener("DOMContentLoaded", carousel());
+var myIndex = 1;
+carousel();
 
 function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {
-        myIndex = 1
-    }
-    x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 3000);
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
 }
